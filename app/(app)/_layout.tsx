4f872399f92fc7ec3e25a1dcdded3ee8ef62a0e6
Elie Francois <eliefrancois22@gmail.com>
2024-09-17 04:07:40 -0400

    Auth is working! Need to set up Sign In Page
    updated gitignore

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,89 @@
+import '~/global.css';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
+import { Redirect, SplashScreen, Stack, Href } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+import { PortalHost } from '@rn-primitives/portal';
+import { ThemeToggle } from '~/components/ThemeToggle';
+import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
+import { SessionProvider, useSession } from '~/context';
+import { Text } from '~/components/ui/text';
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before getting the color scheme.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+  const { session, isLoading } = useSession();
+  
+
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add('bg-background');
+      }
+      if (!theme) {
+        AsyncStorage.setItem('theme', colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+        setAndroidNavigationBar(colorTheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setAndroidNavigationBar(colorTheme);
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+    // You can keep the splash screen open, or render a loading screen like we do here.
+    if (isLoading) {
+      return <Text>Loading...</Text>;
+    }
+  //TODO: if there is no session, redirect to sign-in
+  console.log('session', session);
+  if (!session) {
+    return <Redirect href={"/sign-in" as Href} />;
+  }
+
+  return (
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" redirect={true} />
+      </Stack>
+      <PortalHost />
+    </ThemeProvider>
+  );
+}
